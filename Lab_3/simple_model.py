c5ad198eab3feadf646b574f1525007929fe9467
Kristian Goystdal <krisgoy100@gmail.com>
2024-02-22 10:46:24 +0100

    prep tasks

--- Lab_3/simple_model.py
+++ Lab_3/simple_model.py
@@ -4,9 +4,9 @@ import numpy as np
 muabo = np.genfromtxt("./muabo.txt", delimiter=",")
 muabd = np.genfromtxt("./muabd.txt", delimiter=",")
 
-red_wavelength = 650  # Replace with wavelength in nanometres
-green_wavelength = 550  # Replace with wavelength in nanometres
-blue_wavelength = 470  # Replace with wavelength in nanometres
+red_wavelength = 600  # Replace with wavelength in nanometres
+green_wavelength = 515  # Replace with wavelength in nanometres
+blue_wavelength = 460  # Replace with wavelength in nanometres
 
 wavelength = np.array([red_wavelength, green_wavelength, blue_wavelength])
 
@@ -44,7 +44,42 @@ musr = 100 * (17.6 * (wavelength / 500) ** -4 + 18.78 * (wavelength / 500) ** -0
 
 # TODO calculate penetration depth
 
-print(musr)
-
 penetration_depth = np.sqrt(1 / (3 * mua * (musr + mua)))
-print(penetration_depth)
+print(
+    f"Red: {penetration_depth[0]}, Green: {penetration_depth[1]}, Blue: {penetration_depth[2]}"
+)
+print("")
+
+
+# Transmittion percentage
+finger_depth = 0.015  # Units: m
+C = np.sqrt(3 * mua * (musr + mua))
+transmition = np.exp(-C * finger_depth)
+print(f"Transmition of red waves: {transmition[0]*100} %")
+print(f"Transmation of green waves: {transmition[1]*100} %")
+print(f"Transmition of blue waves: {transmition[2]*100} %")
+print("")
+
+
+# Contrast
+blood_diameter = 300e-6
+transmition_low = np.exp(-C * blood_diameter)
+
+## Variable changes due to higher blood volume fraction
+bvf = 1  # Blood volume fraction, average blood amount in tissue
+mua_other = 25  # Background absorption due to collagen, et cetera
+mua_blood = mua_blood_oxy(wavelength) * oxy + mua_blood_deoxy(  # Absorption due to
+    wavelength
+) * (
+    1 - oxy
+)  # pure blood
+mua = mua_blood * bvf + mua_other
+C = np.sqrt(3 * mua * (musr + mua))
+transmition_high = np.exp(-C * blood_diameter)
+
+
+contrast = abs(transmition_high - transmition_low) / transmition_low
+print(f"Contrast of red waves: {contrast[0]}")
+print(f"Contrast of green waves: {contrast[1]}")
+print(f"Contrast of blue waves: {contrast[2]}")
+print("")
