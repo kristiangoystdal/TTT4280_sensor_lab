7c4d6e160bacd7197324e1c86658674d4ddf30b4
Kristian Goystdal <krisgoy100@gmail.com>
2024-04-18 11:18:08 +0200

    Refactor functions.py to improve readability and performance

--- Lab_2/functions.py
+++ Lab_2/functions.py
@@ -3,6 +3,10 @@ from raspi_import import *
 import os
 import shutil
 
+SQRT_3 = np.sqrt(3)
+PI = np.pi
+DEGREE_FACTOR = 180 / PI
+
 
 def move_bin_files_to_folder(destination_dir):
     if not os.path.exists(destination_dir):
@@ -16,8 +20,8 @@ def move_bin_files_to_folder(destination_dir):
             )
 
 
-def read_file():
-    bin_files_dir = "./bin_files"  # Destination directory for .bin files
+def read_file(folder_name):
+    bin_files_dir = folder_name  # Destination directory for .bin files
     move_bin_files_to_folder(bin_files_dir)
 
     bin_filenames = os.listdir(bin_files_dir)
@@ -39,29 +43,11 @@ def read_file():
     return sample_period, data
 
 
-def kryss_korrelasjon(fs, x, y):
-    N = len(x)
-    lags = np.arange(-N + 1, N)
-    lags_ms = lags / fs * 1000
-    cross = np.correlate(x, y, mode="full")
-
-    return lags, lags_ms, np.abs(cross)
-
-
 def kryss_korrelasjon_list(fs, data):
-    lags = []
-    lags_ms = []
-    cross = []
-    for i in range(3):
-        if i == 0:
-            x = data[1]
-            y = data[0]
-        elif i == 1:
-            x = data[2]
-            y = data[0]
-        else:
-            x = data[2]
-            y = data[1]
+    pairs = [(1, 0), (2, 0), (2, 1)]
+    lags, lags_ms, cross = [], [], []
+    for i, j in pairs:
+        x, y = data[i], data[j]
         N = len(x)
         lags.append(np.arange(-N + 1, N))
         lags_ms.append(lags / fs * 1000)
@@ -70,38 +56,26 @@ def kryss_korrelasjon_list(fs, data):
     return lags, lags_ms, cross
 
 
-def find_delay(cross, N, fs):
-    max_index = np.argmax(np.abs(cross))
-    num_samples_delay = max_index - (N - 1)
-    delay_milliseconds = num_samples_delay / fs * 1000
-
-    return num_samples_delay, delay_milliseconds
-
-
 def find_delay_list(cross, N, fs):
-    delay_ms = []
-    delay_sample = []
+    delay_sample, delay_ms = [], []
     for cross_row in cross:
         max_index = np.argmax(np.abs(cross_row))
         num_samples_delay = max_index - (N - 1)
         delay_sample.append(num_samples_delay)
-        delay_milliseconds = num_samples_delay / fs * 1000
-        delay_ms.append(delay_milliseconds)
+        delay_ms.append(num_samples_delay / fs * 1000)
 
     return delay_sample, delay_ms
 
 
 def angle_finder(lags_ms):
     arc = np.arctan(
-        np.sqrt(3)
-        * (lags_ms[0] + lags_ms[1])
-        / (lags_ms[0] - lags_ms[1] - 2 * lags_ms[2])
+        SQRT_3 * (lags_ms[0] + lags_ms[1]) / (lags_ms[0] - lags_ms[1] - 2 * lags_ms[2])
     )
     if lags_ms[2] > 0:
-        arc += np.pi
-    if -np.pi / 2 < arc < 0:
-        arc += 2 * np.pi
-    degree = arc * 180 / np.pi
+        arc += PI
+    if -PI / 2 < arc < 0:
+        arc += 2 * PI
+    degree = arc * DEGREE_FACTOR
 
     return arc, degree
 
@@ -111,11 +85,11 @@ def std_var(data):
 
 
 def interpolate(x_axis, data):
-    new_x_axis = []
-    for i in range(len(x_axis)):
-        new_x_axis.append(x_axis[i])
-        if i < len(x_axis) - 1:
-            average = (x_axis[i] + x_axis[i + 1]) / 2
-            new_x_axis.append(average)
+    new_x_axis = [
+        average
+        for i in range(len(x_axis) - 1)
+        for average in [x_axis[i], (x_axis[i] + x_axis[i + 1]) / 2]
+    ]
+    new_x_axis.append(x_axis[-1])
 
     return new_x_axis, np.interp(new_x_axis, x_axis, data)
